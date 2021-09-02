aa440e13400591e67b54d7fe98d6f3e0699f6149
mcampora <mcampora@gmail.com>
2021-09-02 17:09:38 +0200

    small fix to push to an ECR public repo

--- Glue2.0/push.sh
+++ Glue2.0/push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 980036580614.dkr.ecr.eu-west-1.amazonaws.com
-docker tag evorra/glue-local:2.0.1 980036580614.dkr.ecr.eu-west-1.amazonaws.com/glue-local:2.0.1
-docker push 980036580614.dkr.ecr.eu-west-1.amazonaws.com/glue-local:2.0.1
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/n4q2f2p4
+docker tag evorra/glue-local:2.0.1 public.ecr.aws/n4q2f2p4/glue-local:2.0.1
+docker push public.ecr.aws/n4q2f2p4/glue-local:2.0.1
