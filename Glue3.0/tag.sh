51fded808e07f672480d6f0dd600a928cde398e2
mcampora <mcampora@gmail.com>
2022-02-17 14:22:12 +0100

    pushed to github

--- Glue3.0/tag.sh
+++ Glue3.0/tag.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-export REPO=public.ecr.aws/evorra
+#export REPO=public.ecr.aws/evorra # AWS
+export REPO=ghcr.io/evorra # GITHUB
 export IMG=glue-local
 export VERSION=3.0.9
 export DST=$REPO/$IMG:$VERSION
